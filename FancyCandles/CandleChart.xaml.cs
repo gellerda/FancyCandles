1ca17387f933086ba4254c3e75716873cc0d7bc2
gellerda <dennisgeller@gmail.com>
2019-10-23 17:24:25 +0300

    Add a copyright and a license notice to every file of the project.

--- FancyCandles/CandleChart.xaml.cs
+++ FancyCandles/CandleChart.xaml.cs
@@ -1,25 +1,33 @@
-﻿using System;
+﻿/* 
+    Copyright 2019 Dennis Geller.
+
+    This file is part of FancyCandles.
+
+    FancyCandles is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    FancyCandles is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FancyCandles.  If not, see<https://www.gnu.org/licenses/>. */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.Globalization;
-using System.Diagnostics;
-using System.Windows.Controls.Primitives;
 using System.ComponentModel;
 using System.Runtime.CompilerServices; // [CallerMemberName]
-using System.Numerics;
 
 namespace FancyCandles
 {
--- FancyCandles/MyWpfMath.cs
+++ FancyCandles/MyWpfMath.cs
@@ -1,6 +1,23 @@
-﻿using System;
+﻿/* 
+    Copyright 2019 Dennis Geller.
+
+    This file is part of FancyCandles.
+
+    FancyCandles is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    FancyCandles is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FancyCandles.  If not, see<https://www.gnu.org/licenses/>. */
+
+using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 
 namespace FancyCandles
 {
